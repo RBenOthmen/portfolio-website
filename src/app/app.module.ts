1408ecbd97f21f02cff489b87e4cf25cdc2d3a28
Rami Ben Othman <ramibenothmen15@gmail.com>
2024-06-28 14:48:23 +0100

    Added Header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
@@ -25,6 +26,7 @@ import { FooterComponent } from './shared/footer/footer.component';
     ProjectsComponent,
     ContactComponent,
     FooterComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
